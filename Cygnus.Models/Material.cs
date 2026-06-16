ae28ee4bb168bd298e5784cb6264621ea3a917c2
Ed Blacker <git@sunships.ltd.uk>
2026-06-16 13:48:12 +0100

    Adding material to the common models

---
+++ Cygnus.Models/Material.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Cygnus.Models;
+public class Material
+{
+    public string? Name { get; set; }
+    public double Velocity { get; set; }
+    public MeasurementUnits MeasurementUnits { get; set; }
+}
