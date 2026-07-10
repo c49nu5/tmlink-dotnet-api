8f5b4143026115f3bc02d78d52b3470936c7aeb0
Ed Blacker <git@sunships.ltd.uk>
2026-07-10 15:20:35 +0100

    Leave connection message history to client apps

--- Cygnus.Interfaces/IConnectionObserver.cs
+++ Cygnus.Interfaces/IConnectionObserver.cs
@@ -5,7 +5,7 @@ namespace Cygnus.Interfaces
     public interface IConnectionObserver
     {
         ConnectionState ConnectionState { get; set; }
-        void ConnectionMessagesChanged(string[] message);
+        void AddConnectionMessage(string message);
         void GaugeDiscovered(IConnectionInformation gauge);
         void GaugeConnected(IGauge? gauge);
     }
