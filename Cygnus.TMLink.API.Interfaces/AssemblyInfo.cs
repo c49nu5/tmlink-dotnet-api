cd63b466cb62ef464681f3dbbcc2d6d42accd2fc
Ed Blacker <git@sunships.ltd.uk>
2026-07-15 17:08:36 +0100

    Allow client code to access the API interfaces without taking a reference to the API

---
+++ Cygnus.TMLink.API.Interfaces/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+[assembly: ExcludeFromCodeCoverage]
+[assembly: InternalsVisibleTo("Cygnus.TMLink.API")]
+[assembly: InternalsVisibleTo("Cygnus.TMLink.API.Tests")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+
