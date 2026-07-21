c24c036b8201bebc7db50882c22cd5ec1f8c7703
Ed Blacker <git@sunships.ltd.uk>
2026-07-21 15:56:11 +0100

    Web-bluetooth suggests lower case for BLE ids

--- Cygnus.TMLink.Interfaces/Constants.cs
+++ Cygnus.TMLink.Interfaces/Constants.cs
@@ -2,23 +2,23 @@
 {
     public class Constants
     {
-        public const string OldCygnusServiceId = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E";
+        public const string OldCygnusServiceId                  = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
 
-        public const string TMLinkServiceId = "DE670901-8025-4C69-A40E-ECCD60563713";
-        public const string TMLinkWriteCommandCharacteristicId = "DE670902-8025-4C69-A40E-ECCD60563713";
-        public const string TMLinkNotifyMessageCharacteristicId = "DE670903-8025-4C69-A40E-ECCD60563713";
-        public const string TMLinkReadMessageCharacteristicId = "DE670904-8025-4C69-A40E-ECCD60563713";
-        public const string TMLinkLiveCharacteristicId = "DE670906-8025-4C69-A40E-ECCD60563713";
-        public const string TMLinkFrozenCharacteristicId = "DE670907-8025-4C69-A40E-ECCD60563713";
+        public const string TMLinkServiceId                     = "de670901-8025-4c69-a40e-eccd60563713";
+        public const string TMLinkWriteCommandCharacteristicId  = "de670902-8025-4c69-a40e-eccd60563713";
+        public const string TMLinkNotifyMessageCharacteristicId = "de670903-8025-4c69-a40e-eccd60563713";
+        public const string TMLinkReadMessageCharacteristicId   = "de670904-8025-4c69-a40e-eccd60563713";
+        public const string TMLinkLiveCharacteristicId          = "de670906-8025-4c69-a40e-eccd60563713";
+        public const string TMLinkFrozenCharacteristicId        = "de670907-8025-4c69-a40e-eccd60563713";
 
-        public const string GenericAccessServiceId = "00001800-0000-1000-8000-00805F9B34FB";
-        public const string DeviceNameCharacteristicId = "00002A00-0000-1000-8000-00805F9B34FB";
+        public const string GenericAccessServiceId              = "00001800-0000-1000-8000-00805f9b34fb";
+        public const string DeviceNameCharacteristicId          = "00002a00-0000-1000-8000-00805f9b34fb";
 
-        public const string DeviceInformationServiceId = "0000180A-0000-1000-8000-00805F9B34FB";
-        public const string DeviceModelCharacteristicId = "00002A24-0000-1000-8000-00805F9B34FB";
-        public const string SerialNumberCharacteristicId = "00002A25-0000-1000-8000-00805F9B34FB";
-        public const string FirmwareRevisionCharacteristicId = "00002A26-0000-1000-8000-00805F9B34FB";
-        public const string SoftwareVersionCharacteristicId = "00002A28-0000-1000-8000-00805F9B34FB";
-        public const string ManufacturerNameCharacteristicId = "00002A29-0000-1000-8000-00805F9B34FB";
+        public const string DeviceInformationServiceId          = "0000180a-0000-1000-8000-00805f9b34fb";
+        public const string DeviceModelCharacteristicId         = "00002a24-0000-1000-8000-00805f9b34fb";
+        public const string SerialNumberCharacteristicId        = "00002a25-0000-1000-8000-00805f9b34fb";
+        public const string FirmwareRevisionCharacteristicId    = "00002a26-0000-1000-8000-00805f9b34fb";
+        public const string SoftwareVersionCharacteristicId     = "00002a28-0000-1000-8000-00805f9b34fb";
+        public const string ManufacturerNameCharacteristicId    = "00002a29-0000-1000-8000-00805f9b34fb";
     }
 }
